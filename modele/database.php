ea8b79c7584823bf9e65bc219344a423fedea169
Delahay Joann <delahay.joann@gmail.com>
2020-12-14 10:54:16 +0100

    init

---
+++ modele/database.php
@@ -0,0 +1,24 @@
+<?php
+namespace modele;
+
+    class database{
+        private $servername = "localhost";
+        private $username = "Joann";
+        private $password = "becode";
+        private $dbname = "chat_php";
+
+        protected function connect(){
+            try {
+                $db = new PDO("mysql:host=$servername;dbname=$dbname;port=3306", $username, $password);
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return $db;
+            }
+            catch(PDOException $e){
+                throw "Connection failed: " . $e->getMessage();
+            }
+        }
+        protected function addMessage(User $from, Message $msg){
+
+        }
+    }
+?>
---
+++ modele/user.php
@@ -0,0 +1,13 @@
+<?php
+namespace modele;
+
+    class User{
+        private $username;
+        private $password;
+        private $joinDate;
+
+        protected function authentification(){
+            
+        }
+    }
+?>
