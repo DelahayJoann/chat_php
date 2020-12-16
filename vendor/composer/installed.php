2c3c6d9c2fc9bc86c9e931c5d31f695ec74d1ad1
Delahay Joann <delahay.joann@gmail.com>
2020-12-16 14:20:21 +0100

    add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '117943c9ab4f43bbcd474c635367ac0d98f416de',
+    'name' => 'dyoan/chat_php',
+  ),
+  'versions' => 
+  array (
+    'dyoan/chat_php' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '117943c9ab4f43bbcd474c635367ac0d98f416de',
+    ),
+  ),
+);
