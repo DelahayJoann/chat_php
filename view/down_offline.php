8b79d24b7e3fe20b6169ba03b354f3c0a8a7c4b7
Thomas Gline <glinethomas20@gmail.com>
2020-12-16 14:29:33 +0100

    add form

--- view/down_offline.php
+++ view/down_offline.php
@@ -8,7 +8,7 @@
         </div>
         <div class="col-12 col-sm-12 col-lg-6 mb-6" id="bandeauDown">
             <p class="message">
-                Veuillez vous connecter <button type="button" class="buttonSignIn">sign in</button> Pas de compte ? Inscrivez-vous dès maintenant <button type="button" class="buttonRegister">register</button>
+                Veuillez vous connecter <button type="button" class="buttonSignIn" data-toggle="modal" data-target="#signin">sign in</button> Pas de compte ? Inscrivez-vous dès maintenant <button type="button" class="buttonRegister" data-toggle="modal" data-target="#register">register</button>
             </p>
         </div>
         <div class="col-12 col-sm-12 col-lg-3 mb-3">
---
+++ view/register.php
@@ -0,0 +1,34 @@
+<?php ob_start(); ?>
+
+
+<div class="modal" id="register">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 class="modal-title">Inscription</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <!-- Modal body -->
+            <div class="modal-body">
+                <form action="index.php" method="post">
+                    <div class="form-group">
+                        <label>email :</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Entrer votre adresse email." required>
+                    </div>
+                    <div class="form-group">
+                        <label>Mot de passe :</label>
+                        <input type="password" class="form-control" id="password" name="password"
+                        placeholder="Entrer un mot de passe" required>
+                    </div>
+                        <input type="submit" class="btn btn-primary" value="submit">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<?php $content = ob_get_clean(); ?>
+<?php require('template.php'); ?>
