cb40e73e11f84791f31f9dd69ce5488c62ca7287
Thomas Gline <glinethomas20@gmail.com>
2020-12-16 13:57:59 +0100

    add views offline

---
+++ view/down_offline.php
@@ -0,0 +1,22 @@
+<?php ob_start(); ?>
+
+
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 col-sm-12 col-lg-3 mb-3">
+        </div>
+        <div class="col-12 col-sm-12 col-lg-6 mb-6" id="bandeauDown">
+            <p class="message">
+                Veuillez vous connecter <button type="button" class="buttonSignIn">sign in</button> Pas de compte ? Inscrivez-vous dès maintenant <button type="button" class="buttonRegister">register</button>
+            </p>
+        </div>
+        <div class="col-12 col-sm-12 col-lg-3 mb-3">
+        </div>
+    </div>
+</div>
+
+
+
+<?php $content = ob_get_clean(); ?>
+<?php require('template.php'); ?>
--- view/top_offline.php
+++ view/top_offline.php
@@ -5,10 +5,7 @@
         <div class="col-12 col-sm-3 col-lg-3 mb-3">
         </div>
         <div class="col-12 col-sm-6 col-lg-6 mb-6" id="bandeauTop">
-            <!--<?= $username ?>-->username 
-            <span>
-                <button type="button" class="buttonLogout">Logout</button>
-            </span>
+            <p class="topOffline">Vous n'êtes pas connecté.</p>
         </div>
         <div class="col-12 col-sm-3 col-lg-3 mb-3">
         </div>
