9a9ad2b2a1aad82c5e2039c96d9c187de7e4fd8f
Delahay Joann <delahay.joann@gmail.com>
2020-12-17 14:17:30 +0100

    correction

--- Model/Database.php
+++ Model/Database.php
@@ -8,7 +8,7 @@ date_default_timezone_set('Europe/Brussels');
                 /* $db = new \PDO("mysql:host=localhost;dbname=chat_php;port=3306", "Joann", "becode");
                 $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
                 return $db; */
-                $db = new PDO("sqlite:".dirname(__FILE__)."\db.db");
+                $db = new PDO("db.db");
                 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 return $db;
             }
