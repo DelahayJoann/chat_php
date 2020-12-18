aebbe955f727544bf153c6e7dcab16e4fba95a3d
Thomas Gline <glinethomas20@gmail.com>
2020-12-18 14:53:15 +0100

    sanitization + modif css

--- view/message.php
+++ view/message.php
@@ -1,13 +1,13 @@
         <!-- Template message utilisateur -->
         <div class="row">
-            <div class="col-12 col-sm-12 col-lg-4 mb-4">
+            <div class="col-12 col-sm-12 col-lg-5 mb-5">
             </div>
-            <div class="col-12 col-sm-12 col-lg-8 mb-8" id="userMessage">
+            <div class="col-12 col-sm-12 col-lg-7 mb-7" id="userMessage">
                 <p id="message">
                     <?= $content ?>
                 </p>
                 <hr>
-                <p>
+                <p class="info">
                     <?= $username ?> | <?= $creationdate ?>
                 </p>
             </div>
--- view/message_other.php
+++ view/message_other.php
@@ -1,15 +1,15 @@
         <!-- Template message other -->
         <div class="row">
-            <div class="col-12 col-sm-12 col-lg-8 mb-8" id="otherMessage">
+            <div class="col-12 col-sm-12 col-lg-7 mb-7" id="otherMessage">
                 <p id="message">
                     <?= $content ?>
                 </p>
                 <hr>
-                <p>
+                <p class="info">
                     <?= $username ?> | <?= $creationdate ?>
                 </p>
             </div>
-            <div class="col-12 col-sm-12 col-lg-4 mb-4">
+            <div class="col-12 col-sm-12 col-lg-5 mb-5">
             </div>
         </div>
         
