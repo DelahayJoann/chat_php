c371899173173bd2509a4cc72e6b2dbede6741af
DelahayJoann <70022234+DelahayJoann@users.noreply.github.com>
2021-03-02 11:34:33 +0100

    Update Database.php

--- Model/Database.php
+++ Model/Database.php
@@ -5,7 +5,7 @@ date_default_timezone_set('Europe/Brussels');
     class Database{
         public static function connect(){
             try {
-                $db = new \PDO("mysql:host=sql2.freemysqlhosting.net;dbname=sql2382667;port=3306", "sql2382667", "xU8!wQ5!");
+                $db = new \PDO("mysql:host=sql2.freemysqlhosting.net;dbname=sql2382667;port=3306", "", "");
                 $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
                 return $db;
             }
